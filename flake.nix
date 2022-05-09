ec952a7adeeb63ea617ddd5f2fe77c6a19e9b194
hugosenari <hugosenari@gmail.com>
2022-05-09 20:40:27 -0300

    chore: re index nesper

--- flake.nix
+++ flake.nix
@@ -7,62 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."nesper-master".dir   = "master";
-  inputs."nesper-master".owner = "nim-nix-pkgs";
-  inputs."nesper-master".ref   = "master";
-  inputs."nesper-master".repo  = "nesper";
-  inputs."nesper-master".type  = "github";
-  inputs."nesper-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."nesper-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."nesper-0_5_0".dir   = "0_5_0";
-  inputs."nesper-0_5_0".owner = "nim-nix-pkgs";
-  inputs."nesper-0_5_0".ref   = "master";
-  inputs."nesper-0_5_0".repo  = "nesper";
-  inputs."nesper-0_5_0".type  = "github";
-  inputs."nesper-0_5_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."nesper-0_5_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."nesper-v0_2_0".dir   = "v0_2_0";
-  inputs."nesper-v0_2_0".owner = "nim-nix-pkgs";
-  inputs."nesper-v0_2_0".ref   = "master";
-  inputs."nesper-v0_2_0".repo  = "nesper";
-  inputs."nesper-v0_2_0".type  = "github";
-  inputs."nesper-v0_2_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."nesper-v0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."nesper-v0_3_0".dir   = "v0_3_0";
-  inputs."nesper-v0_3_0".owner = "nim-nix-pkgs";
-  inputs."nesper-v0_3_0".ref   = "master";
-  inputs."nesper-v0_3_0".repo  = "nesper";
-  inputs."nesper-v0_3_0".type  = "github";
-  inputs."nesper-v0_3_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."nesper-v0_3_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."nesper-v0_4_0".dir   = "v0_4_0";
-  inputs."nesper-v0_4_0".owner = "nim-nix-pkgs";
-  inputs."nesper-v0_4_0".ref   = "master";
-  inputs."nesper-v0_4_0".repo  = "nesper";
-  inputs."nesper-v0_4_0".type  = "github";
-  inputs."nesper-v0_4_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."nesper-v0_4_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."nesper-v0_4_1".dir   = "v0_4_1";
-  inputs."nesper-v0_4_1".owner = "nim-nix-pkgs";
-  inputs."nesper-v0_4_1".ref   = "master";
-  inputs."nesper-v0_4_1".repo  = "nesper";
-  inputs."nesper-v0_4_1".type  = "github";
-  inputs."nesper-v0_4_1".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."nesper-v0_4_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
-  inputs."nesper-v0_6_0".dir   = "v0_6_0";
-  inputs."nesper-v0_6_0".owner = "nim-nix-pkgs";
-  inputs."nesper-v0_6_0".ref   = "master";
-  inputs."nesper-v0_6_0".repo  = "nesper";
-  inputs."nesper-v0_6_0".type  = "github";
-  inputs."nesper-v0_6_0".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."nesper-v0_6_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
